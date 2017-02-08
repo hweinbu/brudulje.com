b3a3fa1077ea8992c6b3fbe853b5273092f67fda
hweinbu <helene@einbu.no>
2017-02-08 19:12:54 +0100

    Løkke som teller til 100

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hallo? Er det noen der ute?");
+console.log("Hallo? Er det noen tær ute?");
+
+// for(var i = 0; i < 10; i = i+1)
+//     console.log(i);
+
+//ignorer dette
+
+var i = 1;
+while(i <= 100){
+    console.log(i);
+
+    if(i < 10){
+        i = i + 1;
+    } else {
+        i = i + 10;
+    }
+}
